3c4ae24c805df15523aac2c12938a0ce4ba03650
Rafiyudistra721 <judihermawan187@gmail.com>
2023-12-02 16:01:49 +0700

    update merge

--- lib/app/routes/app_routes.dart
+++ lib/app/routes/app_routes.dart
@@ -4,11 +4,13 @@ part of 'app_pages.dart';
 abstract class Routes {
   Routes._();
   static const HOME = _Paths.HOME;
+  static const LOGIN = _Paths.LOGIN;
   static const INVEN = _Paths.INVEN;
 }
 
 abstract class _Paths {
   _Paths._();
   static const HOME = '/home';
+  static const LOGIN = '/login';
   static const INVEN = '/inven';
 }
